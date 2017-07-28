3ebe6cdce8b227e9e62cbca582d8387a03d2418d
NhanVo <vothanhnhan.it@gmail.com>
2017-07-28 19:07:29 +0700

    Finish quiz

--- src/com/nhanvo/main/Main.java
+++ src/com/nhanvo/main/Main.java
@@ -8,13 +8,21 @@ public class Main {
 		// Initialize single linked list
 		SingleLinkedList list = new SingleLinkedList();
 		
-		// Append to start linked list
+		// Append to tail linked list
 		list.append(54);
 		list.append(124);
 		list.append(137);
 		list.append(289);
+		list.append(389);
+		list.append(312);
+		list.append(147);
 		
 		// Display data
+		list.display();
+		
+		// Remove tail element
+		System.out.println("Remove all element > 218");
+		list.removeAll(218);
 		list.display();
 	}
 
--- src/com/nhanvo/quiz/SingleLinkedList.java
+++ src/com/nhanvo/quiz/SingleLinkedList.java
@@ -17,22 +17,79 @@ public class SingleLinkedList {
 	
 	// 1. Append an element into the linked list
 	public void append(int data) {
-		Element node = new Element(data, null);
+		Element element = new Element(data, null);
 		size++;
 		
 		// Linked list empty
 		if (head == null){
-			head = node;
+			head = element;
 			tail = head;
-			
+		}
 		// Linked list not empty
-		} else {
-			tail.setLink(node);
-			tail = node;
+		else {
+			tail.setLink(element);
+			tail = element;
 		}
 	}
 	
 	// 2. Remove the tail element from a linked list
+	public void removeTail() {
+		// Linked list empty
+		if (head == null){
+			return;
+		}
+		
+		// Linked list have only one element
+		if (head == tail){
+			head = null;
+			tail = null;
+			size--;
+			return;
+		} 
+			
+		// Linked list have many elements
+		else {
+			Element element = head;
+			while (element.getLink() != tail){
+				element = element.getLink();
+			}
+			tail = element;
+			tail.setLink(null);
+			size--;
+		}
+	}
+	
+	// 3. Remove all element in the linked list that is great than a target value
+	public void removeAll(int data){
+		// Linked list empty
+		if (head == null){
+			return;
+		}
+		
+		// Linked list not empty
+		else {
+			Element element = head;
+			Element prevElement = head;
+			while (element.getLink() != null){
+				// Remove element have data is great than a input data
+				if (element.getData() > data){
+					prevElement.setLink(element.getLink());
+					element = element.getLink();
+					size--;
+					continue;
+				}
+				prevElement = element;
+				element = element.getLink();
+			}
+			
+			// Remove tail if data is great than input data
+			if (tail.getData() > data) {
+				tail = prevElement;
+				tail.setLink(null);
+				size--;
+			}
+		}
+	}
 	
 	
 	/*
@@ -45,21 +102,22 @@ public class SingleLinkedList {
 			return;
 		}
 		
-		// Linked list only have one node
+		// Linked list only have one element
 		if (head.getLink() == null) {
 			System.out.println(head.getData());
 			return;
 		}
-		Element node = head;
-		// Linked list have many node
-		while (size > 0) {
-			System.out.print(node.getData() + " -> ");
-			node = node.getLink();
-			size --;
-		}
 		
-	}
+		// Linked list have many elements
+		Element element = head;
+		System.out.print("List: ");
+		while (element.getLink() != null) {
+			System.out.print(element.getData() + " -> ");
+			element = element.getLink();
+		}
+		System.out.print(tail.getData());
+		System.out.println();
+		System.out.println("Size: " + size);
+	}	
 	
-	
-	// 3. Remove all element in the linked list that is great than a target value
 }
